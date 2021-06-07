4fce866af6f2c3be1a451fef4941f56e57f59f45
Sagar Roy <sagar98roy123@gmail.com>
2021-06-07 23:50:11 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const labelText = 'Enter name:';
+  const buttonText={text:'Click me'};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue',color:'white'}}>{buttonText.text}</button>
     </div>
   );
 }
